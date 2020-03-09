e7c0d41d9d90cda14989b280c41da8d3265e54aa
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2020-03-09 20:49:34 -0300

    feat: add mongo container

--- server/index.js
+++ server/index.js
@@ -1,21 +1,12 @@
+"use strict";
+
 const express = require("express");
-const bodyParser = require("body-parser");
 
+const { PORT = "3001" } = process.env;
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
+app.use((req, res, next) => {
+  res.send("Hello World");
 });
 
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(PORT);
